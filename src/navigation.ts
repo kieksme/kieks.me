cfd0b96ec4e930cc9786633405ab60543c3fc061
André Lademann <alademann@thinkport.digital>
2025-02-02 13:47:50 +0100

    Initial changes

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,8 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
+    /*
     {
       text: 'Homes',
       links: [
@@ -56,6 +57,8 @@ export const headerData = {
         },
       ],
     },
+        
+
     {
       text: 'Landing',
       links: [
@@ -114,12 +117,18 @@ export const headerData = {
       text: 'Widgets',
       href: '#',
     },
+    */
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [
+    /*
+    { text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }
+     */
+  ],
 };
 
 export const footerData = {
   links: [
+    /*
     {
       title: 'Product',
       links: [
@@ -152,32 +161,29 @@ export const footerData = {
         { text: 'Status', href: '#' },
       ],
     },
+    */
     {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Über', href: '#' },
+        { text: 'Impressum', href: '#' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Datenschutzerklärung', href: getPermalink('/datenschutz') },
+    { text: 'Impressumg', href: getPermalink('/impressum') },
   ],
   socialLinks: [
+    /*
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    */
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/nullzueins' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Template made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a>
   `,
 };
